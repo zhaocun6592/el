8f5192cdf7da19ea388473737f5e4805cf653b1d
zhaoyancun <zhaocun6592@126.com>
2017-12-26 18:19:49 +0800

    header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,28 +1,50 @@
 <template>
-  <div class="hello">
-     <div ></div>
-     <div><img :src="seller.avatar" /></div>
+  <div class="header">
+     <div class="content-wrapper">
+       <div class="avatar">
+          <img :src="seller.avatar" />
+       </div>
+       <div class="content">
+          <div class="title">
+               <span class="brand"></span>
+               <span class="name">{{seller.name}}</span>
+          </div>
+          
+      </div>
+     </div>
+     
   </div>
 </template>
 
 <script>
 export default {
-  props:{
-    seller:{
-      type:Object
+  props: {
+    seller: {
+      type: Object
     }
   },
-  data () {
-    return {
-      
-    }
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  div{
-    text-align: center;
+<style scoped lang="scss">
+.header {
+  overflow: hidden;
+  background: rgba(7, 17, 27, 0.5);
+  color: #fff;
+  .content-wrapper {
+    padding: 24px 12px 18px 24px;
+    .avatar {
+      display: inline-block;
+      > img {
+        width: 64px;
+        height: 64px;
+        border-radius: 2px;
+      }
+    }
   }
+}
 </style>
