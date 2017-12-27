0014789ec13d6f647bd8930cc66bc12785a66087
zhaoyancun <zhaocun6592@126.com>
2017-12-27 10:30:46 +0800

    description

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="header">
-     <div class="content-wrapper">
-       <div class="avatar">
-          <img :src="seller.avatar" />
-       </div>
-       <div class="content">
-          <div class="title">
-               <span class="brand"></span>
-               <span class="name">{{seller.name}}</span>
-             </div>
-                <div class="description">
-       {{seller.description}}/{{seller.deliveryTime}}分钟送达
-          </div>
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img :src="seller.avatar" />
       </div>
-     </div>
-     
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
