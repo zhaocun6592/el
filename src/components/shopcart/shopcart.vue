f9c588b54711990e6e281ca13ac2cea50da0f078
zhoayancun <zhaocun6592@126.com>
2018-01-18 23:38:38 +0800

    all

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,44 +1,54 @@
 <template>
-  <div class="shopcart">
-    <div class="content">
-      <div class="content-left">
-        <div class="logo-wrapper">
-          <div class="logo" :class="{'heightlight':count>0}">
-            <i class="icon-shopping_cart" :class="{'heightlight':count>0}" ></i>
+  <div>
+
+    <div class="shopcart">
+      <div class="content" @click="toggle">
+        <div class="content-left">
+          <div class="logo-wrapper">
+            <div class="logo" :class="{'heightlight':count>0}">
+              <i class="icon-shopping_cart" :class="{'heightlight':count>0}"></i>
+            </div>
+            <div class="num" v-show="count>0">{{count}}</div>
           </div>
-          <div class="num" v-show="count>0">{{count}}</div>
+          <div class="price">￥{{totalPrice}}</div>
+          <div class="desc">另需配送费￥{{deliveryPrice}}</div>
         </div>
-        <div class="price">￥{{totalPrice}}</div>
-        <div class="desc">另需配送费￥{{deliveryPrice}}</div>
+        <div class="content-right">
+          <div class="pay" :class="payClass">{{paydesc}}</div>
+        </div>
       </div>
-      <div class="content-right">
-        <div class="pay" :class="payClass">{{paydesc}}</div>
-      </div>
-    </div>
-    <div class="listFood">
-      <div class="list-header">
-        <h1 class="title"物车</h1>
-        <span class="empty">清空</span>
-      </div>
-      <div class="list-content">
-        <ul>
-          <li class="food" v-for="(food,index) in selectFoods">
-              <span class="name">{{food.name}}</span>
-              <div class="price">
+      <transition name="fold">
+        <div class="listFood" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty" @click="empty">清空</span>
+          </div>
+          <div class="list-content" ref="listContent">
+            <ul>
+              <li class="food" v-for="(food,index) in selectFoods">
+                <span class="name">{{food.name}}</span>
+                <div class="price">
                   <span>￥{{food.price*food.count}}</span>
-              </div>
-              <div class="cartcontrol-wrapper">
-                <cartcontrol :food="food"></cartcontrol>
-              </div>
-          </li>
-        </ul>
-      </div>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </transition>
+
     </div>
+    <transition name="fade">
+      <div class="list-mask" @click="hideList" v-show="listShow"></div>
+    </transition>
   </div>
 </template>
 
 <script>
-import cartcontrol from "../cartcontrol/cartcontrol"
+import cartcontrol from "../cartcontrol/cartcontrol";
+import BScroll from "better-scroll";
 export default {
   props: {
     selectFoods: {
@@ -67,7 +77,7 @@ export default {
   data() {
     return {
       msg: "cart",
-      flod:true
+      fold: true
     };
   },
   computed: {
@@ -85,24 +95,59 @@ export default {
       });
       return num;
     },
-    paydesc(){
-      if(this.totalPrice === 0){
-        return `￥${this.minPrice}起配送`
-      }else if(this.totalPrice<this.minPrice){
-          let payWorse =this.minPrice - this.totalPrice
-          return `还差￥${payWorse}元配送`
-      }else{
-          return "去结算"
+    paydesc() {
+      if (this.totalPrice === 0) {
+        return `￥${this.minPrice}起配送`;
+      } else if (this.totalPrice < this.minPrice) {
+        let payWorse = this.minPrice - this.totalPrice;
+        return `还差￥${payWorse}元配送`;
+      } else {
+        return "去结算";
       }
     },
-    payClass(){
-      if(this.totalPrice<this.minPrice){
-        return "not-enough"
-      }else{
-        return "enough"
+    payClass() {
+      if (this.totalPrice < this.minPrice) {
+        return "not-enough";
+      } else {
+        return "enough";
       }
+    },
+    listShow() {
+      if (!this.count) {
+        this.fold = true;
+        return false;
+      }
+      let show = !this.fold;
+      if (show) {
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.listContent, {
+              click: true
+            });
+          } else {
+            this.scroll.refresh();
+          }
+        });
+      }
+      return show;
     }
-
+  },
+  methods: {
+    toggle() {
+      if (this.count) {
+        this.fold = !this.fold;
+      } else {
+        return;
+      }
+    },
+    hideList() {
+      this.fold = !this.fold;
+    },
+    empty(){
+      this.selectFoods.forEach(food => {
+        food.count =0;
+      });
+    }
   }
 };
 </script>
@@ -110,6 +155,26 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "../../assets/stylus/mixin";
+.list-mask {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 40;
+  backdrop-filter: blur(10px);
+  opacity: 1;
+  background: rgba(7, 17, 27, 0.6);
+  &.fade-enter-active,
+  &.fade-leave-active {
+    transition: all 0.5s;
+  }
+  &.fade-enter,
+  &.fade-leave-active {
+    opacity: 0;
+    background: rgba(7, 17, 27, 0);
+  }
+}
 .shopcart {
   position: fixed;
   left: 0;
@@ -143,14 +208,14 @@ export default {
           background: #2b343c;
           border-radius: 50%;
           text-align: center;
-          &.heightlight{
-              background: rgb(0, 160, 220)
+          &.heightlight {
+            background: rgb(0, 160, 220);
           }
           .icon-shopping_cart {
             line-height: 44px;
             font-size: 24px;
             color: #80858a;
-            &.heightlight{
+            &.heightlight {
               color: #fff;
             }
           }
@@ -200,68 +265,78 @@ export default {
         font-size: 12px;
         font-weight: 700;
         background: #2b333b;
-        &.not-enough{
-            background: #2b333b;
+        &.not-enough {
+          background: #2b333b;
         }
-        &.enough{
-            background: #00b43c;
-            color:#fff
+        &.enough {
+          background: #00b43c;
+          color: #fff;
         }
       }
     }
   }
-  .listFood{
-    position:absolute;
+  .listFood {
+    position: absolute;
     left: 0;
-    bottom:0;
-    width:100%;
-    .list-header{
+    top: 0;
+    width: 100%;
+    z-index: -1;
+    transform: translate3d(0, -100%, 0);
+    &.fold-enter-active,
+    &.fold-leave-active {
+      transition: all 0.5s;
+    }
+    &.fold-enter,
+    &.fold-leave-active {
+      transform: translate3d(0, 0, 0);
+    }
+
+    .list-header {
       height: 40px;
       line-height: 40px;
       padding: 0 18px;
-      background:#f3f5f7;
-      border-bottom: 1px solid rgba(7,17,27,0.1);
-      .title{
+      background: #f3f5f7;
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+      .title {
         float: left;
         font-size: 14px;
-        color: rgb(7,17,27);
+        color: rgb(7, 17, 27);
       }
-      .empty{
-        float:right;
+      .empty {
+        float: right;
         font-size: 12px;
-        color: reb(0,160,220);
+        color: reb(0, 160, 220);
       }
     }
-    .list-content{
-      padding:0 18px;
+    .list-content {
+      padding: 0 18px;
       max-height: 217px;
-      overflow:hidden;
+      overflow: hidden;
       background: #fff;
-      .food{
+      .food {
         position: relative;
-        padding:12px 0;
-        box-sizing:border-box;
+        padding: 12px 0;
+        box-sizing: border-box;
         @include border-bottom(rgba(7,17,27,0.1));
-        .name{
-          line-height:24px;
+        .name {
+          line-height: 24px;
           font-size: 14px;
-          color: rgb(7,17,27);
+          color: rgb(7, 17, 27);
         }
-        .price{
+        .price {
           position: absolute;
           right: 90px;
           bottom: 12px;
           line-height: 24px;
           font-size: 14px;
-          font-weight:700;
-          color:rgb(240,20,20);
+          font-weight: 700;
+          color: rgb(240, 20, 20);
         }
-        .cartcontrol-wrapper{
+        .cartcontrol-wrapper {
           position: absolute;
           right: 0;
-          bottom:6px;
+          bottom: 6px;
         }
-
       }
     }
   }
